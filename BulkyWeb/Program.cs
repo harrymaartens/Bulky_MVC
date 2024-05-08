a08deba117a252ecc18427b43b822a2314f11aea
Harry Maartens <hmaartens@hotmail.com>
2024-05-08 18:09:25 +0200

    Section 8: Identity in .NET Core afgerond en alle eerder gemelde problemen zijn opgelost.

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -14,7 +14,15 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
 builder.Services.AddRazorPages();
 // Je moet elke service registreren in de DI container anders krijg je InvalidOperationException
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
